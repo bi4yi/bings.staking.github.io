911722e86ac5f0f2ebfbb60a18a7e8816aaa5536
BTCholder <65149989+bi4yi@users.noreply.github.com>
2022-11-18 17:18:33 +0100

    Update header.php

--- stake/header.php
+++ stake/header.php
@@ -191,6 +191,11 @@
 
 <body>
   
+      <div class="alert">
+  <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>
+  <center><b><font color="red">This platform not yet in production mode , just a demo !</font></b></center>
+</div>
+    
     <!-- HEADER START -->
     <div class="header-line-wrapper">
         <header class="header-wrapper fixed-top plr100">
