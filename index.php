1b8a7d6996e224e07c32271c442b6a9d767d9018
BTCholder <65149989+bi4yi@users.noreply.github.com>
2022-11-19 16:00:39 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php include 'header.php';?>
 <?php include 'content.php';?>
 <?php include 'footer.php';?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Bings staking dapp</title>
+     <meta charset="UTF-8" />
+     <meta http-equiv="refresh" content="3; URL=https://stakingdapp.herokuapp.com/stake/" />
+   </head>
+   <body>
+     <p>This page has been moved. If you are not redirected within 3 seconds, click <a href="https://stakingdapp.herokuapp.com/stake/">here</a> to go to the Bings staking dapp homepage.</p>
+   </body>
+</html>
